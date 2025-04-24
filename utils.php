f6d57b64062d52e3fa13d1c8245e72077465399b
niddumulu <36333121+niddumulu@users.noreply.github.com>
2025-04-24 11:02:21 +0700

    Update utils.php

--- utils.php
+++ utils.php
@@ -1,1 +1,11 @@
+// ===== utils.php =====
+<?php
+function load_json($filename) {
+    if (!file_exists($filename)) return [];
+    return json_decode(file_get_contents($filename), true);
+}
 
+function save_json($filename, $data) {
+    file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
+}
+?>
